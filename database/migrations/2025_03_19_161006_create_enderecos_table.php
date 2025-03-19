ddae33ce5de295877f996593f1f9c309e8c55114
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-03-19 13:49:46 -0300

    correção de migrations

--- database/migrations/2025_03_19_161006_create_enderecos_table.php
+++ database/migrations/2025_03_19_161006_create_enderecos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::create('enderecos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_pessoa')->constrained('pessoas')->onDelete('cascade');
-            $table->string('cep', 10);
+            $table->string('cep', 8);
             $table->string('logradouro', 255);
             $table->string('numero', 20);
             $table->string('complemento', 255)->nullable();
