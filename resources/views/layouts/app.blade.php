82c6e42e6471fb2d0e98683dd6fb61fb87e6d468
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-03-28 12:18:42 -0300

    adição do footer no layout das páginas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,46 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
 
-    <!-- Link para o Bootstrap -->
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-
 </head>
 
-<body class="container mt-4 center">
+<body class="d-flex flex-column min-vh-100">
+
     <!-- Cabeçalho -->
-    <h1 class="text-center mt-4">@yield('header')</h1>
+    <div class="container mt-4">
+        <h1 class="text-center mt-4">@yield('header')</h1>
 
-    <!-- Conteúdo -->
-    @yield('content')
+        <!-- Conteúdo -->
+        @yield('content')
 
-    <!-- Exibição de erros -->
-    @if ($errors->any())
-    <div class="alert alert-danger mt-4">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+        <!-- Exibição de erros -->
+        @if ($errors->any())
+        <div class="alert alert-danger mt-4">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
     </div>
-    @endif
+
+    <!-- Footer -->
+    <footer class="mt-auto">
+        <div class="container d-flex justify-content-center align-items-center">
+            <img src="{{ asset('logo_prodemge.png') }}" alt="Logo Prodemge" width="150" class="me-3">
+            <p class="mb-0">© 2025 - Victor Rocha - Prodemge.</p>
+        </div>
+    </footer>
 
     <!-- Scripts -->
     <script>
         function formatarCPF(event) {
             let input = event.target;
-            let cpf = input.value;
+            let cpf = input.value.replace(/\D/g, ''); // Remove tudo que não for número
 
-
-            if (cpf.match(/[a-zA-Z]/)) {
-                return;
-            }
-
-            cpf = cpf.replace(/\D/g, '');
-
-            // Formata CPF com pontos e traço
             if (cpf.length > 3 && cpf.length <= 6) {
                 cpf = cpf.replace(/(\d{3})(\d{1,3})/, '$1.$2');
             } else if (cpf.length > 6 && cpf.length <= 9) {
@@ -56,18 +58,12 @@
 
             input.value = cpf;
         }
-    </script>
 
-    <script>
-        // Formatação para número de telefone
         document.addEventListener('DOMContentLoaded', function() {
-            var inputTelefone = document.getElementById('telefone'); // Corrigir para telefone
-
+            // Formatação telefone
+            var inputTelefone = document.getElementById('telefone');
             inputTelefone.addEventListener('input', function(event) {
-                var valor = event.target.value;
-
-                valor = valor.replace(/\D/g, ''); // Remove tudo que não for número
-
+                var valor = event.target.value.replace(/\D/g, '');
                 if (valor.length <= 2) {
                     event.target.value = '(' + valor;
                 } else if (valor.length <= 6) {
@@ -77,13 +73,10 @@
                 }
             });
 
-            // Formatação para o CEP
+            // Formatação CEP
             var inputCep = document.getElementById('cep');
             inputCep.addEventListener('input', function(event) {
-                var valorCep = event.target.value;
-
-                valorCep = valorCep.replace(/\D/g, ''); // Remove tudo que não for número
-
+                var valorCep = event.target.value.replace(/\D/g, '');
                 if (valorCep.length <= 5) {
                     event.target.value = valorCep;
                 } else {
@@ -91,29 +84,14 @@
                 }
             });
         });
-    </script>
 
-    <script>
-        // Função para garantir que apenas números sejam inseridos no campo 'numero'
-        function somenteNumeros(event) {
-            let input = event.target;
-            input.value = input.value.replace(/\D/g, ''); // Remove tudo que não for número
-        }
-    </script>
-
-
-    <script>
         function consultarCEP() {
             var cep = document.getElementById('cep').value.replace(/\D/g, '');
-
             if (cep.length === 8) {
-                var url = `https://viacep.com.br/ws/${cep}/json/`; // URL da API viaCEP
-
-                fetch(url)
+                fetch(`https://viacep.com.br/ws/${cep}/json/`)
                     .then(response => response.json())
                     .then(data => {
                         if (!data.erro) {
-                            // preenche os campos do formulário com os dados retornados
                             document.getElementById('logradouro').value = data.logradouro;
                             document.getElementById('bairro').value = data.bairro;
                             document.getElementById('cidade').value = data.localidade;
@@ -123,7 +101,6 @@
                         }
                     })
                     .catch(error => {
-                        console.error('Erro ao consultar o CEP:', error);
                         alert('Erro ao consultar o CEP.');
                     });
             } else {
