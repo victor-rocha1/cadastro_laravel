43b1612a6661f1e21bbbbf35b43591b4bd1099ae
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-04-02 09:20:04 -0300

    correção na busca de cpf no banco de dados
    O CPF está salvo com a máscara 000.000.000-00, então, não precisa remover os caracteres . e -

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,9 +15,11 @@ class HomeController extends Controller
             $pesquisa = trim($request->input('pesquisar'));
 
             $pessoas = Pessoa::whereRaw("LOWER(nome) LIKE ?", ["%" . strtolower($pesquisa) . "%"])
-                ->orWhereRaw("REPLACE(cpf, '.', '') LIKE ?", ["%" . str_replace(['.', '-'], '', $pesquisa) . "%"])
+            ->orWhere('cpf', 'LIKE', "%$pesquisa%")
                 ->orderBy('nome') // ordena os nomes em ordem alfabética
                 ->get();
+                
+            // dd($pessoas);
 
             if ($pessoas->isEmpty()) {
                 $erro = "Nenhuma pessoa encontrada para: $pesquisa";
