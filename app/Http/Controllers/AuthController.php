3ee976561e2dbd5f3af034ab2c4e6d90cf5ea50d
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-05-29 11:14:25 -0300

    finalizando método de logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -54,7 +54,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        Auth::logout();
+        Auth::guard('web')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
